3281070fcfa242cb3ae866d253dccf2574fa7bf9
cristiandoring <cristiandoring@gmail.com>
2024-07-15 10:24:19 -0300

    Função para iniciar cronômetro quando clicar no botão iniciar

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
+var segundos = 0;
+var minutos = 0;
+var milisegundos = 0;
+
+
 function iniciar(){
-    console.log("iniciando...")
+    //chama a função contador a cada 0.1s (100ms)
+    setInterval(cronometro,100)
 }
 
 function pausar(){
@@ -8,4 +14,36 @@ function pausar(){
 
 function zerar(){
     console.log("zerando...")
+}
+
+function cronometro(){
+    milisegundos++
+    if(milisegundos == 60){
+        segundos++
+        milisegundos=0
+    }
+
+    if(segundos == 60){
+        minutos++
+        segundos=0
+    }
+    
+
+    
+    document.getElementById('relogio').innerText = minutos + ":" + segundos + ":" + milisegundos
+
+}
+
+function formatar(){
+    if(milisegundos <10){
+        milisegundos = "0" + milisegundos
+    }
+   
+    if(segundos <10){
+        segundos = "0" + segundos
+    }
+
+    if(minutos <10){
+        minutos = "0" + minutos
+    }
 }
