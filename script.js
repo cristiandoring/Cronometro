33c4e432f48a737d1e8039f10df509b9cb7a14a2
cristiandoring <cristiandoring@gmail.com>
2024-07-16 11:11:00 -0300

    Desabilitar vários cliques no botão iniciar

--- script.js
+++ script.js
@@ -5,19 +5,32 @@ var intervalo
 
 
 function iniciar(){
+
+    cronometro()
     //chama a função contador a cada 1s (1000ms)
-     intervalo = setInterval(cronometro,0.001)
+     intervalo = setInterval(cronometro,1000)
+
+     document.querySelector('.iniciar').disabled = true;
+
+     //desabilita o botão iniciar após clicar 1x nele
 }
 
 function pausar(){
     //pausa o tempo do cronômetro quando chama a função intervalo
     clearInterval(intervalo)
+
+    //habilita o botão iniciar após clicar no pause
+    document.querySelector('.iniciar').disabled = false;
 }
 
 function zerar(){
     clearInterval(intervalo)
+    minutos = segundos = horas = 0
     document.getElementById('relogio').innerText = "00:00:00"
     intervalo=0;
+
+    //habilita o botão iniciar após clicar no zerar
+    document.querySelector('.iniciar').disabled = false;
 }
 
 function cronometro(){
@@ -32,10 +45,9 @@ function cronometro(){
         minutos=0
     }
     
-
-    
     document.getElementById('relogio').innerText = formatar(horas) + ":" + formatar(minutos) + ":" + formatar(segundos)
 
+    
 }
 
 function formatar(digito){
